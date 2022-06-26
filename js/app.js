ce5efb8340f8ad5d9fdf6fc5bd970a629331c046
AgusDiP <agusdipietro@live.com.ar>
2022-06-26 19:04:13 +0200

    arreglando index, creando db.json, empezando con los eventList del formulario y agregando validacion y alertas

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let cliente ={
+    mesa:'',
+    hora:'',
+    pedido:[]
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click',guardarCliente);
+
+function guardarCliente(){
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    //Revisar si hay campos vacios
+    const camposVacios = [mesa, hora].some(campo => campo ==='');
+
+    if(camposVacios){
+        //Verif que ya haya una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta){
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback','d-block','text-center');
+            alerta.textContent= 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove()
+            }, 2500);
+        }
+        return
+    }
+
+        console.log('todos los campos estan llenos')
+}

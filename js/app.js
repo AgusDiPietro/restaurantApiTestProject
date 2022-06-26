c0ad2d67606062dfe2778e39fca00d5c2e8096ac
AgusDiP <agusdipietro@live.com.ar>
2022-06-26 20:00:46 +0200

    Mostrando contenido en el html despues de la validacion, conectar con el API y mostrar info dentro de API

--- js/app.js
+++ js/app.js
@@ -30,6 +30,47 @@ function guardarCliente(){
         }
         return
     }
+  //asignamos datos del formulario a una copia del objeto cliente.
+   cliente = {...cliente,mesa,hora}
 
-        
+   //ocultar modal
+   const modalFormulario = document.querySelector('#formulario');
+   const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+   modalBootstrap.hide();  
+
+   //Mostrar secciones
+   mostrarSecciones();
+
+   //Obtener platillos de la API Json-server
+   obtenerPlatillos();
+
+}
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'))
+}
+function obtenerPlatillos(){
+    const url = "http://localhost:4000/platillos";
+
+    fetch(url)
+       .then(respuesta=> respuesta.json())
+       .then(resultado => mostrarPlatillos(resultado))
+       .catch( error => console.log(error));
+}
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach(platillo =>{
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        const nombre = document.createElement('div');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        row.appendChild(nombre);
+        contenido.append(row);
+    })
+
 }
